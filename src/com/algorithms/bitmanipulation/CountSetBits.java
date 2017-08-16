fa7b443b799d09405c7295e2928437470552aae9
Krishna Sai Mulpuri <mulpk6@gmail.com>
2017-08-16 13:19:24 -0400

    added program to count set bits in a number

---
+++ src/com/algorithms/bitmanipulation/CountSetBits.java
@@ -0,0 +1,20 @@
+package com.algorithms.bitmanipulation;
+
+/**
+ * Created by sai on 8/16/2017.
+ * Aim is to count number of 1's in binary representation of a given integer
+ */
+public class CountSetBits {
+    public static int countOnes(int number) {
+        if(number == 0)
+            return 0;
+        else
+            return (number & 1) + countOnes(number >> 1);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(countOnes(1023));
+        System.out.println(countOnes(127));
+
+    }
+}
